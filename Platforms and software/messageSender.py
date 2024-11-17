25de34d5423e643ceef9a8a122b83f0c609d1220
Coolpeople3 <159194319+Coolpeople3@users.noreply.github.com>
2024-11-17 16:59:05 -0500

    Update messageSender.py

--- Platforms and software/messageSender.py
+++ Platforms and software/messageSender.py
@@ -1,10 +1,21 @@
 import pyautogui as auto
 import time
 
-rep = int(input("How many times, sir? "))
-text = input("What shall we send, sir? ")
+print("Welcome to Hitarth's Auto Messenger!")
+print("Instructions:")
+print("1. Answer the prompts below.")
+print("2. After starting, click on the typing area in a messaging app where you want the messages to be sent.")
+print("3. The script will automatically send the messages as per your input.\n")
+
+rep = int(input("How many times should the message be sent? "))
+text = input("What message should be sent? ")
+
+print("\nStarting in 5 seconds. Get ready!")
+time.sleep(5)
 
 for i in range(rep):
     auto.write(text)
     auto.press('enter')
     time.sleep(1)
+
+input("\nMessages sent! Press Enter to exit.")
