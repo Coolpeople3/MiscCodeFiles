648e41f5cda661980902b2a4341495d7b91006b9
Coolpeople3 <159194319+Coolpeople3@users.noreply.github.com>
2024-11-19 12:46:44 -0500

    removed unnecessary lines

--- Pranks/Open URL.py
+++ Pranks/Open URL.py
@@ -1,8 +1,5 @@
 import webbrowser
 a=input("what URL should I open?")
 while 1<2:
-  # Specify the URL you want to open
   url = a
-  # Open a new tab in the default web browser
   webbrowser.open_new_tab(url)
-print("Thanks")
